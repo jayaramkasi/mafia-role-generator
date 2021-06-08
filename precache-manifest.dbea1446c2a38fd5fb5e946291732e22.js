09b0625fe1b02abfe4555c48997aa4b5d03cd078
Jayaram Kasi visweswaran <jayaram@Jayarams-MacBook-Pro.local>
2021-06-08 23:00:51 +0530

    Updates

---
+++ precache-manifest.dbea1446c2a38fd5fb5e946291732e22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639d7561d1ec6fd1da10807eb849f78c",
+    "url": "/mafia-role-allocator/index.html"
+  },
+  {
+    "revision": "35db8bb3801fa41e2367",
+    "url": "/mafia-role-allocator/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "5eb56c5389a5212dc07e",
+    "url": "/mafia-role-allocator/static/css/main.0d0c13ed.chunk.css"
+  },
+  {
+    "revision": "35db8bb3801fa41e2367",
+    "url": "/mafia-role-allocator/static/js/2.9384eb1a.chunk.js"
+  },
+  {
+    "revision": "d6b29286b0bbae6e17db84419d44fb6a",
+    "url": "/mafia-role-allocator/static/js/2.9384eb1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb56c5389a5212dc07e",
+    "url": "/mafia-role-allocator/static/js/main.041c91f9.chunk.js"
+  },
+  {
+    "revision": "0e3fd079afb75af73f81",
+    "url": "/mafia-role-allocator/static/js/runtime-main.05452ef8.js"
+  }
+]);
