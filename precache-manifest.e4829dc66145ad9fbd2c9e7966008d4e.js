7530840fdd52d775b024117433c1948b707ea35e
Jayaram Kasi visweswaran <jayaram@Jayarams-MacBook-Pro.local>
2021-06-08 23:21:35 +0530

    Updates

--- precache-manifest.e4829dc66145ad9fbd2c9e7966008d4e.js
+++ precache-manifest.e4829dc66145ad9fbd2c9e7966008d4e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eabeecf77eb0e44be7105323e807ee62",
+    "revision": "c47a80277cc99d9a94bfe7186c02278b",
     "url": "/mafia-role-generator/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mafia-role-generator/static/css/2.9233ae65.chunk.css"
   },
   {
-    "revision": "ae35aa517fdb63973c14",
+    "revision": "438f38ad11b75f7b9a96",
     "url": "/mafia-role-generator/static/css/main.0d0c13ed.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mafia-role-generator/static/js/2.9384eb1a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ae35aa517fdb63973c14",
-    "url": "/mafia-role-generator/static/js/main.ccc35e42.chunk.js"
+    "revision": "438f38ad11b75f7b9a96",
+    "url": "/mafia-role-generator/static/js/main.3f315136.chunk.js"
   },
   {
     "revision": "f911f5b0e3d097f5122c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mafia-role-generator/precache-manifest.aab9728428d66653bf3e3a04d21cf2dd.js"
+  "/mafia-role-generator/precache-manifest.e4829dc66145ad9fbd2c9e7966008d4e.js"
 );
 
 self.addEventListener('message', (event) => {
